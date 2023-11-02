3d8651abbcbe96a42aee7a529495284fb9fabd41
adebu <c.ubedacuadrado@gmail.com>
2023-11-02 17:35:46 +0100

    add loop to load periods of time into the db(crashing when loading all at once)

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 from downloadFiles import get_training_data, extract_values, connection_to_DB
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
-start_date = "2023-01-01T00:00:00"
-end_date = "2023-10-01T00:00:00"
+start_date = "2019-01-01T00:00:00"
+end_date = "2023-01-01T00:00:00"
 
 training_data = ['demand', 'wind', 'solar', 'spot_price']
-historic_data = ['demand_historic', 'wind_historic', 'solar_historic', 'spot_historic']
+historic_data = ['demand_historic', 'wind_historic', 'solar_historic']
 
 # Convert start_date and end_date to datetime objects
 start_date = datetime.fromisoformat(start_date)
 end_date = datetime.fromisoformat(end_date)
+current_date = start_date
 
-while start_date <= end_date:
+while current_date < end_date:
     # Calculate the end of the current month
-    next_month = start_date.replace(day=1, hour=0, minute=0, second=0) + timedelta(days=32)
-    end_of_month = (next_month - timedelta(days=next_month.day)).replace(hour=23, minute=59, second=59)
+    query_year = current_date+ relativedelta(years = 1)
 
     # Format end_of_month in the "%Y-%m-%dT%H:%M:%S" format
-    formatted_end_of_month = end_of_month.strftime("%Y-%m-%dT%H:%M:%S")
+    formatted_end_of_month = start_date.strftime("%Y-%m-%dT%H:%M:%S")
     for item in training_data:
         try:
-            generation_data = get_training_data(start_date, end_date, item)
+            generation_data = get_training_data(current_date, query_year, item)
             df = extract_values(generation_data, item)
-            #df.to_csv(f'{item}.csv')
+
             connection_to_DB(df, item)
         except:
             print(f'Values were not found for {item} for the month{formatted_end_of_month}')
 
-    start_date = end_of_month + timedelta(seconds=1)
+    current_date = current_date + relativedelta(years = 1)
 
 
 
