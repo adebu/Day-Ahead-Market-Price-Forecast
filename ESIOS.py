8fccad3f02e9eae4b2eaa4f302dce5aaa050ce04
adebu <c.ubedacuadrado@gmail.com>
2023-12-02 19:27:22 +0100

    add gas to database

--- ESIOS.py
+++ ESIOS.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import os
 
 class ESIOS:
     def __init__(self):
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from downloadFiles import get_training_data, extract_values, connection_to_DB
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from ESIOS import ESIOS
+from mibgas import mibgas
 
 start_date = "2019-01-01T00:00:00"
 end_date = "2023-01-01T00:00:00"
@@ -9,16 +10,16 @@ end_date = "2023-01-01T00:00:00"
 training_data = ['demand', 'wind', 'solar', 'spot_price']
 historic_data = ['demand_historic', 'wind_historic', 'solar_historic']
 
-# Convert start_date and end_date to datetime objects
+# convert start_date and end_date to datetime objects
 start_date = datetime.fromisoformat(start_date)
 end_date = datetime.fromisoformat(end_date)
 current_date = start_date
 
 while current_date < end_date:
-    # Calculate the end of the current month
+    # calculate the end of the current month
     query_year = current_date+ relativedelta(years = 1)
 
-    # Format end_of_month in the "%Y-%m-%dT%H:%M:%S" format
+    # format end_of_month in the "%Y-%m-%dT%H:%M:%S" format
     formatted_end_of_month = start_date.strftime("%Y-%m-%dT%H:%M:%S")
     for item in training_data:
         try:
@@ -32,6 +33,10 @@ while current_date < end_date:
     current_date = current_date + relativedelta(years = 1)
 
 
+# load gas data
+years = [2019, 2020, 2021, 2022, 2023]
 
-#Load marginalpdbf files into db
-#test_df = df_to_db(dates)
+for year in years:
+    gas_data = mibgas.get_data(year)
+    gas_data_cleaned = mibgas.extract_data(gas_data)
+    gas_data_cleaned.to_csv(f'gas_data_{year}.csv')
