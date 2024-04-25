432e290589e83350d20430674341e351d4036418
adebu <c.ubedacuadrado@gmail.com>
2024-04-25 14:31:07 +0200

    Restructure files

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
+
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
 app = Dash(__name__)
