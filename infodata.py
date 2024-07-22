ce5348ee91ddc21f52bb5bcc3429d7da626f3ff9
adebu <c.ubedacuadrado@gmail.com>
2024-07-22 16:19:41 +0200

    update developer branch

--- infodata.py
+++
@@ -1,6 +0,0 @@
-indicators ={'demand':1775, 'wind':1777, 'solar': 1779, 'spot_price': 600, 'nuclear': 474}
-url = "https://api.esios.ree.es/indicators/{}?start_date={}&end_date={}{}"
-features = ['demand', 'wind', 'solar', 'spot_price', 'nuclear']
-reference_day_esios = "2018-12-31"
-reference_hour_esios = "23:00"
-start_date = '2019-01-01'
